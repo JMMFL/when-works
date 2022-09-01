327435758a5193090aef41e5b3efecd986a90ff7
JMMFL <mehravarj+github@gmail.com>
2022-09-01 17:02:11 -0400

    Displayed current time in seconds since epoch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <div>Hello World</div>;
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    async function getTime() {
+      const response = await fetch("/time");
+      const data = await response.json();
+      setCurrentTime(data.time);
+    }
+
+    getTime();
+  }, []);
+
+  return <div>Current time is {currentTime}</div>;
 }
