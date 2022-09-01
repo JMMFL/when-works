d713b98a48f818d6a86f263fa59822119dbb70da
Zamil Bahri <zamilbahri7@gmail.com>
2022-09-01 18:53:10 -0400

    added imports and boilerplate

--- api/api.py
+++ api/api.py
@@ -1,8 +1,10 @@
 from flask import Flask
-import time
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 @app.route('/time')
 def get_current_time():
-	return {'time': time.time()}
+	return {'time': datetime.now()}
