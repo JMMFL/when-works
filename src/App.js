df8b20dba8355d8f5874fd31fb1bd275b1dd0b36
JMMFL <mehravarj+github@gmail.com>
2022-09-17 17:17:42 -0500

    Refactored for Date type instead of JSON

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import { useState } from "react";
 import Calendar from "react-calendar";
 import CalendarBtn from "./components/CalendarBtn";
 import "./style.css";
+import datesEqual from "./utils/datesEqual";
+import datesInclude from "./utils/datesInclude";
 
 export default function App() {
   const [dates, setDates] = useState([]);
 
-  const toggleDate = (value) => {
-    const date = value.toJSON();
-    const newDates = dates.includes(date)
-      ? dates.filter((d) => d !== date)
+  const toggleDate = (date) => {
+    const dateIsOn = datesInclude(dates, date);
+    const newDates = dateIsOn
+      ? dates.filter((d) => !datesEqual(d, date))
       : [...dates, date];
-
     setDates(newDates);
   };
 
   const tileClassName = ({ date }) => {
-    if (dates.includes(date.toJSON())) {
-      return "myClass";
-    }
+    if (datesInclude(dates, date)) return "myClass";
   };
 
   return (
