ea1a796a0b9b6652f27727fc0bd71a517f9bae92
JMMFL <mehravarj+github@gmail.com>
2022-09-30 11:24:57 -0500

    Added timeOptions (I forgot)

---
+++ src/utils/timeOptions.js
@@ -0,0 +1,21 @@
+const timeOptions = {
+  hours: [
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+  ],
+
+  minutes: ["00", "30"],
+  meridiem: ["AM", "PM"],
+};
+
+export default timeOptions;
