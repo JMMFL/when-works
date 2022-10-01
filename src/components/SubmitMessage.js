8ea50f54e8e3b4accb10227f79925d060e77e0fc
JMMFL <mehravarj+github@gmail.com>
2022-10-01 13:41:26 -0500

    Added SubmitMessage

---
+++ src/components/SubmitMessage.js
@@ -0,0 +1,24 @@
+import useHostContext from "../hooks/useHostContext";
+
+export default function SubmitMessage() {
+  const { data } = useHostContext();
+  console.log(data);
+
+  return (
+    <>
+      <h1>Thank you for your time</h1>
+      {Object.entries(data).map(([id, timeBlocks]) => (
+        <div key={id}>
+          <h2>{id}</h2>
+          {timeBlocks.map(({ start, end }, index) => (
+            <p key={index}>
+              {start.values.hours}:{start.values.minutes}
+              {start.values.meridiem} to {end.values.hours}:{end.values.minutes}
+              {end.values.meridiem}
+            </p>
+          ))}
+        </div>
+      ))}
+    </>
+  );
+}
