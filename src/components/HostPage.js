03264ae8d62774fdd167e0ede07a45f50400673e
JMMFL <mehravarj+github@gmail.com>
2022-10-01 13:40:01 -0500

    Added state to show/hide screens

--- src/components/HostPage.js
+++ src/components/HostPage.js
@@ -1,15 +1,50 @@
-import useHostContext from "../hooks/useHostContext";
+import { useReducer } from "react";
 import CalendarForm from "./CalendarForm";
+import SubmitMessage from "./SubmitMessage";
 import TimeForm from "./TimeForm";
 
+function reducer(state, action) {
+  let newState;
+  switch (action) {
+    case "ToggleForms": {
+      newState = {
+        ...state,
+        calendar: !state.calendar,
+        times: !state.times,
+      };
+
+      break;
+    }
+
+    case "ShowSubmitMessage": {
+      newState = {
+        calendar: false,
+        times: false,
+        submit: true,
+      };
+
+      break;
+    }
+
+    default:
+      throw new Error();
+  }
+
+  return newState;
+}
+
 export default function HostPage() {
-  const { data } = useHostContext();
-  console.log(data);
+  const [{ display }, setDisplay] = useReducer(reducer, {
+    calendar: true,
+    times: false,
+    submit: false,
+  });
 
   return (
     <>
-      <CalendarForm />
-      <TimeForm />
+      {display.calendar && <CalendarForm setDisplay={setDisplay} />}
+      {display.times && <TimeForm setDisplay={setDisplay} />}
+      {display.submit && <SubmitMessage />}
     </>
   );
 }
