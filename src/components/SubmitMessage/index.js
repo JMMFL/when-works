82ebd7cbba3e6063301602c1f274c9f0215c6039
JMMFL <mehravarj+github@gmail.com>
2022-10-14 10:17:33 -0500

    Added copy links to SubmitMessage

--- src/components/SubmitMessage/index.js
+++ src/components/SubmitMessage/index.js
@@ -1,12 +1,17 @@
 import useHostContext from "../../hooks/useHostContext";
+import CopyLink from "./CopyLink";
 
 export default function SubmitMessage() {
   const { data } = useHostContext();
   console.log(data);
 
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <h1>Thank you for your time</h1>
-    </>
+      <p>Use the links below to manage your event</p>
+      <CopyLink label="Invite Link" url="www.whenworks.com/xg29/invite" />
+      <CopyLink label="Results Link" url="www.whenworks.com/xg29/results" />
+      <CopyLink label="Edit Link" url="www.whenworks.com/xg29/edit" />
+    </div>
   );
 }
